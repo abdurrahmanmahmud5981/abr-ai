7b229983fd318d2c6dc8afe2252ef66217cba0be
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-01 23:22:50 +0600

    feat: add testimonials section with dynamic content and integrate testimonial data

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "@/components/hero";
 import { Card, CardContent } from "@/components/ui/card";
 import { features } from "@/data/features";
 import { howItWorks } from "@/data/howItWorks";
+import { testimonial } from "@/data/testimonial";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -75,6 +77,47 @@ export default function Home() {
                   <h3 className="text-xl font-semibold mt-4">{item.title}</h3>
                   <p className="text-muted-foreground mt-2">{item.description}</p>
                 </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+      {/* testimonials */}
+      <section className="w-full py-12  md:py-24 lg:py-32 bg-muted/50">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">What Our Users Say</h2>
+          <div className="grid grid-cols-1  md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+            {
+              testimonial.map((testimonial, index) => (
+                <Card key={index} className="bg-background">
+                  <CardContent className="flex flex-col items-center text-center pt-6">
+                    <div className=" flex flex-col space-y-4">
+                      <div className="flex items-center justify-start text-start space-x-4">
+                        <div className="relative w-12 h-12 flex-shrink-0">
+                          <Image
+                            src={testimonial.image}
+                            alt={testimonial.author}
+                            width={50}
+                            height={50}
+                            className="rounded-full object-cover border-2 border-primary/20"
+                          />
+                        </div>
+                        <div className="">
+                          <p className=" font-semibold ">{testimonial.author}</p>
+                          <p className="text-muted-foreground text-sm">{testimonial.role} at {testimonial.company}</p>
+                        </div>
+                      </div>
+                      <blockquote >
+                        <p className="text-muted-foreground italic relative text-start">
+                          <span className="text-3xl text-primary absolute -top-4 -left-5 "> &quot;</span>
+                          {testimonial.quote}
+                          <span className="text-3xl text-primary absolute -bottom-4"> &quot;</span>
+                        </p>
+                      </blockquote>
+
+                    </div>
+                  </CardContent>
+                </Card>
               ))
             }
           </div>
