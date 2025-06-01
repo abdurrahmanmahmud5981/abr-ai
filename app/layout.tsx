81a80ffc47eeffab6d5c692b6891402a6e77c5a2
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-01 12:32:32 +0600

    feat: add initial configuration for PostCSS and Tailwind CSS, including custom theme colors and animations
    feat: add SVG icons for various components including file, globe, next, vercel, and window
    
    chore: initialize TypeScript configuration with strict settings and module resolution

---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+const inter = Inter({ subsets: ["latin"] })
+
+
+export const metadata: Metadata = {
+  title: "Abrai - Mentor Of Your Career",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+
+
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }} >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header></Header>
+            <main className="min-h-screen">
+              {children}
+            </main>
+            {/* footer */}
+            <footer className="bg-muted/50 py-12 text-center">
+              <div className="container mx-auto">
+                <p className="text-sm text-gray-200">
+                  © {new Date().getFullYear()} ABRAI. All rights reserved.
+                </p>
+              </div>
+
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/header/Header.tsx
@@ -0,0 +1,83 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full border-b bg-background/70 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 ">
+      <nav className=" container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href={`/`}>ABRAI</Link>
+        <div className=" flex items-center gap-4">
+          <SignedIn>
+            <Link href={`/dashboard`}>
+              <Button variant={"outline"} className="flex items-center gap-2">
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:block ">Industry Insights</span>
+              </Button>
+            </Link>
+
+            
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button>
+                    <StarsIcon className="h-4 w-4" />
+                    <span className="hidden md:block ">Growth Tools</span>
+                    <ChevronDown className=" h-4 w-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <Link href={`/resume`} className="flex items-center gap-2">
+                      <FileText className="h-4 w-4" />
+                      <span >Build Resume</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href={`/ai-cover-letter`} className="flex items-center gap-2">
+                      <PenBox className="h-4 w-4" />
+                      <span >Cover Letter</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href={`/interview`} className="flex items-center gap-2">
+                      <GraduationCap className="h-4 w-4" />
+                      <span >Interview Preparation</span>
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+          </SignedIn>
+          <SignedIn>
+            <UserButton appearance={
+              {
+                elements:{
+                  avatarBox: "h-10 w-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "text-semibold",
+                }
+              }
+
+            } 
+            afterSwitchSessionUrl="/"
+            afterSignOutUrl="/"
+            />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button variant={"outline"}>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+
+        </div>
+   
+
+    </nav>
+
+    </header >
+  )
+}
+
+export default Header
