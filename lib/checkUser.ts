81403ae884f041fe1382fbdf8d1646eb7b11c591
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-02 13:38:43 +0600

    feat: enhance user authentication by adding user retrieval and creation logic

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,8 +1,29 @@
 import { currentUser } from "@clerk/nextjs/server"
+import { db } from "./prisma";
 
 export const checkUser = async () => {
     const user = await currentUser();
     if (!user) {
         throw new Error("User not authenticated");
+    }
+    try {
+        const loggedInUser = await db.user.findUnique({ where: { clerkUserId: user.id } });
+        if (loggedInUser) {
+             return loggedInUser;
+        }
+       
+        const name = `${user.firstName} ${user.lastName}`;
+        const newUser = await db.user.create({
+            data: {
+                clerkUserId: user.id,
+                name,
+                imageUrl: user.imageUrl,
+                email: user.emailAddresses[0]?.emailAddress,
+            },
+        });
+        return newUser;
+
+    } catch (error) {
+        throw new Error(error instanceof Error ? error.message : "An error occurred while checking user");
     }
 }
