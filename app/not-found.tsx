b78c6f30cdce5beaeff1acfc50f36e2d98fbe654
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-03 21:16:54 +0600

    fix: remove unnecessary class from NotFound component's error message

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
         <h1 className="text-6xl gradient-title font-bold">404 - Page Not Found</h1>
-        <p className="mt-4 text-popover-foreground">Sorry, the page you are looking for does not exist.</p>
+        <p className="mt-4 ">Sorry, the page you are looking for does not exist.</p>
         <p className="mt-4">You can go back to the <Link href="/"><Button>home page</Button></Link>.</p>
     </div>
   )
