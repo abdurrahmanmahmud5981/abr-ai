3fc0b1d66c7a317d7408de657157df03ea17dfc0
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-04 14:23:38 +0600

    feat: update OnboardingPage to check user onboarding status asynchronously

--- app/(main)/onboarding/page.tsx
+++ app/(main)/onboarding/page.tsx
@@ -1,8 +1,16 @@
 import { industries } from '@/data/industries'
 import React from 'react'
 import OnboardingForm from './_components/onboarding-form'
+import { getUserOnboardingStatus } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
-const OnboardingPage = () => {
+const OnboardingPage = async () => {
+  //check if user is onboarded
+  const { isOnboarded } = await getUserOnboardingStatus()
+
+  if(isOnboarded){
+    redirect('/dashboard')
+  }
   return (
     <div>
       <OnboardingForm industries={industries} />
