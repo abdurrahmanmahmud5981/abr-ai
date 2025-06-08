6b81e0747645dd567e94c8f431202efe04f623ac
Abdur Rahman (work) <ar7543078@gmail.com>
2025-06-08 21:04:59 +0600

    feat: improve onboarding form structure and enhance sub-industry selection logic

--- app/(main)/onboarding/_components/onboarding-form.tsx
+++ app/(main)/onboarding/_components/onboarding-form.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { onboardingSchema } from '@/lib/schema';
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { onboardingSchema } from "@/lib/schema";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select'
-import { Label } from '@/components/ui/label'
-const OnboardingForm: React.FC = ({ industries }) => {
-  const [selectedIndustry, setSelectedIndustry] = React.useState(null)
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+const OnboardingForm: React.FC = ({ industries }: { industries: string[] }) => {
+  const [selectedIndustry, setSelectedIndustry] = React.useState(null);
 
   const router = useRouter();
   const {
@@ -17,32 +30,48 @@ const OnboardingForm: React.FC = ({ industries }) => {
     handleSubmit,
     formState: { errors },
     setValue,
-    watch
+    watch,
   } = useForm({
-    resolver: zodResolver(onboardingSchema)
-  })
+    resolver: zodResolver(onboardingSchema),
+  });
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
 
   const watchIndustry = watch("industry");
   console.log("Selected watch Industry:", watch("subIndustry"));
+  const submitForm = async () => {
+    console.log("Form submitted");
+  };
   return (
-    <div className='flex items-center justify-center bg-background'>
+    <div className="flex items-center justify-center bg-background">
       <Card className="w-full max-w-lg mt10 mx-2">
         <CardHeader>
-          <CardTitle className="gradient-title text-4xl">Complete Onboarding</CardTitle>
-          <CardDescription>Fill in the details below to get started.</CardDescription>
+          <CardTitle className="gradient-title text-4xl">
+            Complete Onboarding
+          </CardTitle>
+          <CardDescription>
+            Fill in the details below to get started.
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form className="space-y-6" onSubmit={handleSubmit(submitForm)}>
+            {/* industry */}
             <div className="space-y-4">
-              <Label htmlFor="industry" className="block mb-2">Industry</Label>
+              <Label htmlFor="industry" className="block mb-2">
+                Industry
+              </Label>
               <Select
-               onValueChange={(value) => {
+                onValueChange={(value) => {
                   // Reset sub-industry when industry changes
                   console.log("Selected Industry Value:", value);
-                  console.log("Selected Industry Object:", industries.find((ind) => ind.id === value));
+                  console.log(
+                    "Selected Industry Object:",
+                    industries.find((ind) => ind.id === value),
+                  );
                   setValue("industry", value);
                   setSelectedIndustry(
-                    industries.find((ind) => ind.id === value)
+                    industries.find((ind) => ind.id === value),
                   );
                   setValue("subIndustry", "");
                 }}
@@ -51,48 +80,77 @@ const OnboardingForm: React.FC = ({ industries }) => {
                   <SelectValue placeholder="Select Industry" />
                 </SelectTrigger>
                 <SelectContent>
-                  {industries.map(industry => <SelectItem key={industry.id} value={industry.id}>{industry.name}</SelectItem>)}
-                </SelectContent>
-              </Select>
-              {
-                errors.industry && (
-                  <p className="text-red-500 text-sm">{errors.industry.message}</p>
-                )
-              }
-            </div>
-          {
-            watchIndustry && (
-                <div className="space-y-4 mt-4">
-              <Label htmlFor="subIndustry" className="block mb-2">Sub Industry</Label>
-              <Select
-                onValueChange={(value) => {
-                  setValue('subIndustry', value);
-                }}
-              >
-                <SelectTrigger id="subIndustry">
-                  <SelectValue placeholder="Select Sub Industry" />
-                </SelectTrigger>
-                <SelectContent>
-                  {selectedIndustry?.subIndustries.map(ind => (
-                    <SelectItem key={ind} value={ind}>{ind}</SelectItem>
+                  {industries.map((industry) => (
+                    <SelectItem key={industry.id} value={industry.id}>
+                      {industry.name}
+                    </SelectItem>
                   ))}
                 </SelectContent>
               </Select>
-              {
-                errors.subIndustry && (
-                  <p className="text-red-500 text-sm">{errors.subIndustry.message}</p>
-                )
-              }
+              {errors.industry && (
+                <p className="text-red-500 text-sm">
+                  {errors.industry.message}
+                </p>
+              )}
             </div>
-            )
-          }
-            
-         
+            {/* sub industry */}
+            {watchIndustry && (
+              <div className="space-y-4 mt-4">
+                <Label htmlFor="subIndustry" className="block mb-2">
+                  Sub Industry
+                </Label>
+                <Select
+                  onValueChange={(value) => {
+                    setValue("subIndustry", value);
+                  }}
+                >
+                  <SelectTrigger id="subIndustry">
+                    <SelectValue placeholder="Select Sub Industry" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {selectedIndustry?.subIndustries.map((ind) => (
+                      <SelectItem key={ind} value={ind}>
+                        {ind}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                {errors.subIndustry && (
+                  <p className="text-red-500 text-sm">
+                    {errors.subIndustry.message}
+                  </p>
+                )}
+              </div>
+            )}
+            {/* years of experience */}
+
+            <div className="space-y-4 mt-4">
+              <Label htmlFor="experience" className="block mb-2">
+                Years of Experience
+              </Label>
+              <Input
+                id="experience"
+                type="number"
+                min={0}
+                max={50}
+                placeholder="Enter years of experience"
+                {...register("experience")}
+              />
+              {errors.experience && (
+                <p className="text-red-500 text-sm">
+                  {errors.experience.message}
+                </p>
+              )}
+            </div>
+
+        
+
+            {/*  */}
           </form>
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default OnboardingForm
+export default OnboardingForm;
