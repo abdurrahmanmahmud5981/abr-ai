e18e5f488a2217746ae172690404b952c9d1e6f0
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-06-15 22:16:24 +0600

    feat: add Quiz component and integrate it into MockInterviewPage with navigation enhancements

--- app/(main)/interview/layout.tsx
+++ app/(main)/interview/layout.tsx
@@ -1,13 +1,11 @@
 import React, { Suspense } from 'react'
 import { BarLoader } from 'react-spinners'
-const DashboardLayout = ({ children }:{
+const InterviewdLayout = ({ children }:{
     children:React.ReactNode
 }) => {
     return (
         <div className="px-5 py-8">
-            <div className="flex items-center justify-between mb-5">
-                <h1 className='text-6xl font-bold gradient-title'>Industry Insights </h1>
-            </div>
+            
             <Suspense fallback={
                 <BarLoader className='mt-4' width={"100%"} color='gray'/>
             }>
@@ -17,4 +15,4 @@ const DashboardLayout = ({ children }:{
     )
 }
 
-export default DashboardLayout
+export default InterviewdLayout;
--- app/(main)/interview/mock/page.tsx
+++ app/(main)/interview/mock/page.tsx
@@ -1,8 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import Quiz from '../_components/Quiz'
 
 const MockInterviewPage = () => {
   return (
-    <div>MockInterviewPage</div>
+    <div>
+
+      <div className="">
+        <Link href={'/interview'}>
+          <Button variant={'link'} className='gap-2 pl-0'>
+            <ArrowLeft className='h-4 w-4' />
+            Back to Interview Preparation
+          </Button>
+        </Link>
+      </div>
+
+      <div >
+        <h1 className='text-6xl font-bold gradient-title'>Mock Interview </h1>
+        <p className='text-muted-foreground'>
+          Test Your Knowledge with Industry-specific Questions
+        </p>
+      </div>
+
+      <Quiz />
+    </div>
   )
 }
 
