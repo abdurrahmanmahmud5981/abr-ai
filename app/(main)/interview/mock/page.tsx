b7b7581502a8c9f5875e4e65f4bd30c9e21e705f
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-06-16 21:39:16 +0600

    feat: enhance Quiz component layout and integrate loading state with user instructions

--- app/(main)/interview/mock/page.tsx
+++ app/(main)/interview/mock/page.tsx
@@ -6,24 +6,24 @@ import Quiz from '../_components/Quiz'
 
 const MockInterviewPage = () => {
   return (
-    <div>
+    <div className='container mx-auto space-y-4 py-6'>
 
-      <div className="">
+      <div className="flex flex-col space-y-2 mx-2">
         <Link href={'/interview'}>
           <Button variant={'link'} className='gap-2 pl-0'>
             <ArrowLeft className='h-4 w-4' />
             Back to Interview Preparation
           </Button>
         </Link>
-      </div>
 
-      <div >
-        <h1 className='text-6xl font-bold gradient-title'>Mock Interview </h1>
-        <p className='text-muted-foreground'>
-          Test Your Knowledge with Industry-specific Questions
-        </p>
-      </div>
 
+        <div >
+          <h1 className='text-6xl font-bold gradient-title'>Mock Interview </h1>
+          <p className='text-muted-foreground'>
+            Test Your Knowledge with Industry-specific Questions
+          </p>
+        </div>
+      </div>
       <Quiz />
     </div>
   )
