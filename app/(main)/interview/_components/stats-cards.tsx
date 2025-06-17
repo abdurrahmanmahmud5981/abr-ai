3ca1c1a38d90ffd9012da62ac3d5fe677b1e081d
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-06-17 11:52:11 +0600

    feat: enhance StatsCards component to display average score, total questions, and latest score

--- app/(main)/interview/_components/stats-cards.tsx
+++ app/(main)/interview/_components/stats-cards.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Brain, Trophy } from 'lucide-react';
 import React from 'react'
 
 const StatsCards = ({ assessments }) => {
@@ -21,7 +23,51 @@ const StatsCards = ({ assessments }) => {
 
     return (
         <div className='grid gap-4 md:grid-cols-3'>
+            {/* average score */}
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className='text-sm font-medium'>Average Score</CardTitle>
+                    <Trophy className='w-4 h-4 text-muted-foreground' />
 
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">{getAverageScore()}%</div>
+                    <p className="text-xs text-muted-foreground">
+                        Across all assessments
+                    </p>
+
+                </CardContent>
+            </Card>
+            {/* questions */}
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className='text-sm font-medium'>Questions Practiced</CardTitle>
+                    <Brain className='w-4 h-4 text-muted-foreground' />
+
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">{getTotalQuestions()}</div>
+                    <p className="text-xs text-muted-foreground">
+                        Total Questions
+                    </p>
+
+                </CardContent>
+            </Card>
+            {/* lates score  */}
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className='text-sm font-medium'>Latest Score</CardTitle>
+                    <Trophy className='w-4 h-4 text-muted-foreground' />
+
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">{getLatestAssessment()?.quizScore.toFixed(1) || 0}%</div>
+                    <p className="text-xs text-muted-foreground">
+                        Most Recent Quiz
+                    </p>
+
+                </CardContent>
+            </Card>
         </div>
     )
 }
