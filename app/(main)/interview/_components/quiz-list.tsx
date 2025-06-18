42ef980059b085565dc484d061760f58f192cc23
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-06-18 22:53:27 +0600

    feat: implement QuizList component with layout and navigation for starting new quizzes

--- app/(main)/interview/_components/quiz-list.tsx
+++ app/(main)/interview/_components/quiz-list.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const QuizList = ({assessments}) => {
+  const router = useRouter();
+  const [selected, setSelected] = useState(null)
   return (
-    <div>
-      
-    </div>
+    <>
+      <Card>
+      <CardHeader className='flex justify-between items-center'>
+      <div className="">
+          <CardTitle className='gradient-title text-3xl md:text-4xl'>
+          Recent Quizzes 
+        </CardTitle>
+        <CardDescription>Review Your Past Quiz Performance </CardDescription>
+      </div>
+      <Button onClick={()=> router.push("/interview/mock")}>Start New Quiz</Button>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+
+    </Card>
+    {/* dialog */}
+    </>
   )
 }
 
