6d6a874745f874758f68de75c11663c54106fd4e
abdurrahmanmahmud5981 <ar7543078@gmail.com>
2025-06-19 19:37:32 +0600

    feat: add schemas for onboarding, contact info, entry, and resume validation

--- lib/schema.ts
+++ lib/schema.ts
@@ -16,4 +16,46 @@ export const onboardingSchema = z.object({
         ),
     skills: z.string().transform((val) => val ? val.split(",").map(skill => skill.trim()).filter(Boolean) : undefined),
     
+});
+
+
+
+// contact info 
+export const contactSchema = z.object({
+    email:z.string().email("Invalid Email Address"),
+    mobile:z.string().optional(),
+    linkedin:z.string().optional(),
+    twitter:z.string().optional(),
+})
+
+
+// entry 
+export const entrySchema = z.object({
+    title:z.string().min(1,"Title is required"),
+    organization:z.string().min(1,"Organization is required"),
+    startDate:z.string().min(1,"Start Date is required"),
+    endDate:z.string().optional(),
+    description:z.string().min(1,"Description is required"),
+    current:z.boolean().default(false),
+}).refine((data)=>{
+    if(!data.current && !data.endDate){
+        return false
+    }
+    return true;
+},
+{
+    message:"End date is required unless this is your current position",
+    path:["endDate"],
+}
+)
+
+
+// 
+export const resumeSchema = z.object({
+    contactInfo:contactSchema,
+    summary:z.string().min(1,"Professional summary is required"),
+    skills:z.string().min(1,"Skills are required"),
+    experience:z.array(entrySchema),
+    education:z.array(entrySchema),
+    project:z.string(entrySchema),
 })
